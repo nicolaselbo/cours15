72aa3f5a32800924ccfc51c218749e6c7f427ce0
MatthieuSchmit <matth.sht@gmail.com>
2016-12-19 20:44:35 +0100

    Add files via upload

---
+++ vue/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<header>
+    <h1>ISFCE</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php?p=listC">Listes des cours</a></li>
+    </ul>
+</nav>
